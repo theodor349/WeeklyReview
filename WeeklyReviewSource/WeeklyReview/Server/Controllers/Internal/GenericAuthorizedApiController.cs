a704cdbea48df304558842cc3028ced5389d381e
Theodor Risager <theodor349@gmail.com>
2023-08-31 21:18:34 +0200

    UserGuid always returns same guid

--- WeeklyReviewSource/WeeklyReview/Server/Controllers/Internal/GenericAuthorizedApiController.cs
+++ WeeklyReviewSource/WeeklyReview/Server/Controllers/Internal/GenericAuthorizedApiController.cs
@@ -8,7 +8,7 @@ namespace WeeklyReview.Server.Controllers.Internal
     [Route("api/v{version:apiVersion}/[controller]")]
     public class GenericAuthorizedApiController : ControllerBase
     {
-        public Guid UserGuid => Guid.NewGuid();
+        public Guid UserGuid => new Guid("24fe9480-4e7a-4515-b96c-248171496591");
 
         public GenericAuthorizedApiController()
         {
