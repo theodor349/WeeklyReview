4bb8aba66692ab1c6f7bf4a5f52f8e515cca406c
Theodor Risager <theodor349@gmail.com>
2023-09-21 16:15:45 +0200

    Refactor

--- WeeklyReviewSource/WeeklyReview/Client/Services/ApiWeeklyReviewService.cs
+++ WeeklyReviewSource/WeeklyReview/Client/Services/ApiWeeklyReviewService.cs
@@ -33,20 +33,17 @@ namespace WeeklyReview.Client.Services
 
         public async Task<EntryModel?> Create(EnterEntryModel model, Guid userGuid)
         {
-            var task = await _client.POSTAsync<EntryModel, EnterEntryModel>($"/api/v1/Entry/Enter", model, CancellationToken.None);
-            return task;
+            return await _client.POSTAsync<EntryModel, EnterEntryModel>($"/api/v1/Entry/Enter", model, CancellationToken.None);
         }
 
         public async Task<EntryModel?> Get(int key, Guid userGuid)
         {
-            var task = await _client.GETAsync<EntryModel>($"/api/v1/Entry/{key}", CancellationToken.None);
-            return task;
+            return await _client.GETAsync<EntryModel>($"/api/v1/Entry/{key}", CancellationToken.None);
         }
 
         public async Task<IEnumerable<EntryModel>> GetAll(Guid userGuid)
         {
-            var task = await _client.GETCollectionAsync<EntryModel>($"/api/v1/Entry", CancellationToken.None);
-            return task;
+            return await _client.GETCollectionAsync<EntryModel>($"/api/v1/Entry", CancellationToken.None);
         }
     }
 
@@ -61,36 +58,27 @@ namespace WeeklyReview.Client.Services
 
         public async Task<ActivityChangeModel> ChangeActivity(int sourceKey, int destinationKey, Guid userGuid)
         {
-            var task = await _client.POSTAsync<ActivityChangeModel, object>($"/api/v1/Activity/{sourceKey}/ChangeTo/{destinationKey}", null, CancellationToken.None);
-
-            return task;
+            return await _client.POSTAsync<ActivityChangeModel, object>($"/api/v1/Activity/{sourceKey}/ChangeTo/{destinationKey}", null, CancellationToken.None);
         }
 
         public async Task<ActivityChangeModel> Delete(int key, Guid userGuid)
         {
-            var task = await _client.DELETEAsync<ActivityChangeModel, object>($"/api/v1/ActivityChange/{key}", CancellationToken.None);
-
-            return task;
+            return await _client.DELETEAsync<ActivityChangeModel, object>($"/api/v1/ActivityChange/{key}", CancellationToken.None);
         }
 
         public async Task<ActivityChangeModel?> Get(int key, Guid userGuid)
         {
-            var task = await _client.GETAsync<ActivityChangeModel>($"/api/v1/ActivityChange/{key}", CancellationToken.None);
-
-            return task;
+            return await _client.GETAsync<ActivityChangeModel>($"/api/v1/ActivityChange/{key}", CancellationToken.None);
         }
 
         public async Task<IEnumerable<ActivityChangeModel>> GetAll(Guid userGuid)
         {
-            var task = await _client.GETCollectionAsync<ActivityChangeModel>($"/api/v1/ActivityChange", CancellationToken.None);
-
-            return task;
+            return await _client.GETCollectionAsync<ActivityChangeModel>($"/api/v1/ActivityChange", CancellationToken.None);
         }
 
         public async Task RollBackActivityChange(int key, Guid UserGuid)
         {
-            var task = await _client.POSTAsync<CategoryModel, object>($"/api/v1/ActivityChange/{key}/Rollback", null, CancellationToken.None);
-
+            await _client.POSTAsync<CategoryModel, object>($"/api/v1/ActivityChange/{key}/Rollback", null, CancellationToken.None);
         }
     }
 
@@ -105,30 +93,22 @@ namespace WeeklyReview.Client.Services
 
         public async Task<CategoryModel> ChangeColor(int key, Color color, Guid userGuid)
         {
-            var task = await _client.POSTAsync<CategoryModel, Color>($"/api/v1/Category/{key}/ChangeColor", color, CancellationToken.None);
-
-            return task;
+            return await _client.POSTAsync<CategoryModel, Color>($"/api/v1/Category/{key}/ChangeColor", color, CancellationToken.None);
         }
 
         public async Task<CategoryModel> Delete(int key, Guid userGuid)
         {
-            var task = await _client.DELETEAsync<CategoryModel, object>($"/api/v1/Category/{key}", CancellationToken.None);
-
-            return task;
+            return await _client.DELETEAsync<CategoryModel, object>($"/api/v1/Category/{key}", CancellationToken.None);
         }
 
         public async Task<CategoryModel?> Get(int key, Guid userGuid)
         {
-            var task = await _client.GETAsync<CategoryModel>($"/api/v1/Category/{key}", CancellationToken.None);
-
-            return task;
+            return await _client.GETAsync<CategoryModel>($"/api/v1/Category/{key}", CancellationToken.None);
         }
 
         public async Task<IEnumerable<CategoryModel>> GetAll(Guid userGuid)
         {
-            var task = await _client.GETCollectionAsync<CategoryModel>($"/api/v1/Category", CancellationToken.None);
-
-            return task;
+            return await _client.GETCollectionAsync<CategoryModel>($"/api/v1/Category", CancellationToken.None);
         }
     }
 
@@ -143,31 +123,22 @@ namespace WeeklyReview.Client.Services
 
         public async Task<ActivityChangeModel> Convert(int sKey, int dKey, Guid userGuid)
         {
-            var task = await _client.POSTAsync<ActivityChangeModel, object>($"/api/v1/Activity/{sKey}/ChangeTo/{dKey}", null, CancellationToken.None);
-
-            return task;
+            return await _client.POSTAsync<ActivityChangeModel, object>($"/api/v1/Activity/{sKey}/ChangeTo/{dKey}", null, CancellationToken.None);
         }
 
         public async Task<ActivityModel> Delete(int key, Guid userGuid)
         {
-            var task = await _client.DELETEAsync<ActivityModel, object>($"/api/v1/Activity/{key}", CancellationToken.None);
-
-            return task;
+            return await _client.DELETEAsync<ActivityModel, object>($"/api/v1/Activity/{key}", CancellationToken.None);
         }
 
         public async Task<ActivityModel?> Get(int key, Guid userGuid)
         {
-            var task = await _client.GETAsync<ActivityModel>($"/api/v1/Activity/{key}", CancellationToken.None);
-
-            return task;
+            return await _client.GETAsync<ActivityModel>($"/api/v1/Activity/{key}", CancellationToken.None);
         }
 
         public async Task<IEnumerable<ActivityModel>> GetAll(Guid userGuid)
         {
-            // TODO: This Does Not Parse Correctly 
-            var task = await _client.GETCollectionAsync<ActivityModel>($"/api/v1/Activity", CancellationToken.None);
-
-            return task;
+            return await _client.GETCollectionAsync<ActivityModel>($"/api/v1/Activity", CancellationToken.None);
         }
     }
 }
