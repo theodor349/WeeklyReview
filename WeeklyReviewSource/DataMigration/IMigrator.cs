44d227ae7ba15780ca8fb1144a1eaf649add5c8c
Theodor Risager <theodor349@gmail.com>
2023-10-30 23:56:03 +0100

    Removed Migration Project

--- WeeklyReviewSource/DataMigration/IMigrator.cs
+++
@@ -1,4 +0,0 @@
-﻿public interface IMigrator
-{
-    Task Migrate();
-}
--- WeeklyReviewSource/DataMigration/Models/EntryModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DataMigration.Models
-{
-    public class EntryModel
-    {
-        public List<string> Entries { get; set; } = new List<string>();
-        public DateTime Date { get; set; }
-    }
-}
