cebe23ca64d12f683072f75dc686300e9aee2d42
Theodor Risager <theodor349@gmail.com>
2024-03-26 18:56:18 +0100

    Finished Enter Activity Functionality

--- WeeklyReviewSource/React-Frontend/Weekly-Review/src/components/enterActivityFrom/enterActivityFrom.tsx
+++ WeeklyReviewSource/React-Frontend/Weekly-Review/src/components/enterActivityFrom/enterActivityFrom.tsx
@@ -14,6 +14,7 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+
 import { Input } from "@/components/ui/input";
 import { format } from "date-fns"
 import { Calendar as CalendarIcon } from "lucide-react";
@@ -23,10 +24,20 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover"
+import { get } from "http";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 const formSchema = z.object({
   date: z.date(),
-  activity: z.string().array(),
+  activities: z.string().array(),
+  socials: z.string().array(),
 });
 
 export function EnterActivityFrom() {
@@ -34,8 +45,9 @@ export function EnterActivityFrom() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      date: new Date(),
-      activity: [""],
+      date: getDefaultDate(),
+      activities: [""],
+      socials: [""],
     },
   });
 
@@ -46,45 +58,151 @@ export function EnterActivityFrom() {
     console.log(values);
   }
   
+  function addActivity() {
+    form.setValue("activities", [...form.getValues("activities"), ""])
+  }
+
+  function removeActivity() {
+    const activities = form.getValues("activities")
+    if(activities.length === 1) 
+      return
+    form.setValue("activities", activities.slice(0, activities.length - 1))
+  }
+
+  function addMinutes(minutes: number) {
+    const date = form.getValues("date")
+    form.setValue("date", new Date(date.getTime() + minutes * 60000))
+  }
+
+  function resetDate() {
+    form.setValue("date", getDefaultDate())
+  }
+
+  function getDefaultDate(){
+    var date = new Date();
+    var minutes = date.getMinutes();
+    var change = minutes % 15;
+    date.setMinutes(minutes - change);
+    return date;
+  }
+
+  function updateDate(date: Date) {
+    const oldDate = form.getValues("date");
+    date.setHours(oldDate.getHours());
+    date.setMinutes(oldDate.getMinutes());
+    form.setValue("date", date);
+  }
+
+  function addSocial() {
+    form.setValue("socials", [...form.getValues("socials"), ""])
+  }
+
+  function removeSocial() {
+    const socials = form.getValues("socials")
+    if(socials.length === 1) 
+      return
+    form.setValue("socials", socials.slice(0, socials.length - 1))
+  }
+
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="date"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Date</FormLabel>
-              <FormControl>
-              <Popover>
-                <PopoverTrigger asChild>
-                  <Button
-                    variant={"outline"}
-                    className={cn(
-                      "w-[280px] justify-start text-left font-normal",
-                      !{...field} && "text-muted-foreground"
-                    )}
-                  >
-                    <CalendarIcon className="mr-2 h-4 w-4" />
-                    {field.value ? format(field.value, "MMM d, yyyy") : "Select a date"}
-                  </Button>
-                </PopoverTrigger>
-                <PopoverContent className="w-auto p-0">
-                  <Calendar
-                    mode="single"
-                    selected={field.value}
-                    onSelect={field.onChange}
-                    initialFocus
-                  />
-                </PopoverContent>
-              </Popover>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+    <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>Enter Activity</CardTitle>
+        <CardDescription>Fill in the form below to enter an activity</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="date"
+              render={({ field: date }) => (
+                <>
+                  <FormItem>
+                    <FormControl>
+                      <Popover>
+                        <PopoverTrigger asChild>
+                          <Button
+                            variant={"outline"}
+                            className={cn(
+                              "w-[280px] justify-start text-left font-normal",
+                              !{...date} && "text-muted-foreground"
+                            )}
+                          >
+                            <CalendarIcon className="mr-2 h-4 w-4" />
+                            {date.value ? format(date.value, "MMM d, yyyy HH:mm") : "Select a date"}
+                          </Button>
+                        </PopoverTrigger>
+                        <PopoverContent className="w-auto p-0">
+                          <Calendar
+                            mode="single"
+                            selected={date.value}
+                            onSelect={(date:Date) => {updateDate(date)}}
+                            initialFocus
+                          />
+                        </PopoverContent>
+                      </Popover>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                  <Button variant={"secondary"} onClick={() => addMinutes(-15)}>-15</Button>
+                  <Button variant={"secondary"} onClick={() => resetDate()}>Reset</Button>
+                  <Button variant={"secondary"} onClick={() => addMinutes(15)}>+15</Button>
+                </>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="activities"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Activities</FormLabel>
+                  {field.value.map((activities, index) => (
+                    <FormField
+                      control={form.control}
+                      name={`activities.${index}`}
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormControl>
+                            <Input {...field} />
+                          </FormControl>
+                        </FormItem>
+                      )}
+                    />
+                  ))}
+                  <Button variant={"secondary"} onClick={() => removeActivity()}>Remove</Button>
+                  <Button variant={"secondary"} onClick={() => addActivity()}>Add</Button>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="socials"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Socials</FormLabel>
+                  {field.value.map((socials, index) => (
+                    <FormField
+                      control={form.control}
+                      name={`socials.${index}`}
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormControl>
+                            <Input {...field} />
+                          </FormControl>
+                        </FormItem>
+                      )}
+                    />
+                  ))}
+                  <Button variant={"secondary"} onClick={() => removeSocial()}>Remove</Button>
+                  <Button variant={"secondary"} onClick={() => addSocial()}>Add</Button>
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
   );
 }
