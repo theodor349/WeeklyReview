c54f85be28b0fab93e05da98325e487e006901a4
Theodor Risager <theodor349@gmail.com>
2024-07-05 17:24:48 +0200

    New Combobox

--- WeeklyReviewSource/weekly-review-client/src/components/entryForm/combobox.tsx
+++ WeeklyReviewSource/weekly-review-client/src/components/entryForm/combobox.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import {useCombobox} from 'downshift'
 import * as React from "react";
 import { Check, ChevronsUpDown } from "lucide-react";
 import { cn } from "@/lib/utils";
@@ -31,40 +32,80 @@ type Props = {
   selection: string[];
 };
 
+function getFilter(inputValue: string) {
+  const lowerCasedInputValue = inputValue.toLowerCase()
+
+  return function filter(inputValue: string) {
+    return (
+      !inputValue ||
+      inputValue.toLowerCase().includes(lowerCasedInputValue)
+    )
+  }
+}
+
 export default function Combobox(
   { form, entry, index, placeholder, selection }: Props,
 ) {
-  const [open, setOpen] = React.useState(false);
-  const [inputValue, setInputValue] = React.useState("");
+  const [items, setItems] = React.useState(selection)
+  const {
+    isOpen,
+    getToggleButtonProps,
+    getLabelProps,
+    getMenuProps,
+    getInputProps,
+    highlightedIndex,
+    getItemProps,
+    selectedItem,
+  } = useCombobox({
+    onInputValueChange({inputValue}) {
+      setItems(selection.filter(getFilter(inputValue)))
+    },
+    items,
+    itemToString(item) {
+      return item ? item : ''
+    },
+  })
 
-  return (
-    <>
-      <Command>
-        <CommandInput placeholder={placeholder} onFocus={() => {setOpen(true)}}/>
-        <Popover open={open} onOpenChange={setOpen}>
-          <PopoverTrigger>
-          </PopoverTrigger>
-          <PopoverContent className="min-w-32">
-            <CommandGroup>
-              <CommandList>
-                {selection.map((value) => (
-                  <CommandItem
-                    key={value}
-                    value={value}
-                    onSelect={(currentValue) => {
-                      setInputValue(currentValue);
-                      form.setValue(`${entry}[${index}]`, currentValue);
-                      setOpen(false);
-                    }}
-                  >
-                    {value}
-                  </CommandItem>
-                ))}
-              </CommandList>
-            </CommandGroup>
-          </PopoverContent>
-        </Popover>
-      </Command>
-    </>
-  );
+    return (
+      <div>
+        <div className="flex flex-col gap-1">
+          <div className="flex shadow-sm bg-white gap-0.5">
+            <input
+              placeholder={placeholder}
+              className="w-full p-1.5"
+              {...getInputProps()}
+            />
+            <button
+              aria-label="toggle menu"
+              className="px-2"
+              type="button"
+              {...getToggleButtonProps()}
+            >
+              {isOpen ? <>&#8593;</> : <>&#8595;</>}
+            </button>
+          </div>
+        </div>
+        <ul
+          className={`absolute w-full max-w-sm bg-white mt-1 shadow-md max-h-80 overflow-scroll p-0 z-10 ${
+            !(isOpen && items.length) && 'hidden'
+          }`}
+          {...getMenuProps()}
+        >
+          {isOpen &&
+            items.map((item, index) => (
+              <li
+                className={cn(
+                  highlightedIndex === index && 'bg-blue-300',
+                  selectedItem === item && 'font-bold',
+                  'py-2 px-3 shadow-sm flex flex-col',
+                )}
+                key={item}
+                {...getItemProps({item, index})}
+              >
+                <span>{item}</span>
+              </li>
+            ))}
+        </ul>
+      </div>
+    )
 }
