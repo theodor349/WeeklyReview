e7e8cb1091d2afcc699cbbe76648b8216c4023be
Theodor Risager <theodor349@gmail.com>
2024-07-05 16:13:18 +0200

    Update Form

--- WeeklyReviewSource/weekly-review-client/src/components/entryForm/combobox.tsx
+++ WeeklyReviewSource/weekly-review-client/src/components/entryForm/combobox.tsx
@@ -1,24 +1,23 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { Check, ChevronsUpDown } from "lucide-react"
-import { cn } from "@/lib/utils"
-import { ControllerRenderProps, FieldValues } from 'react-hook-form'
-import { Input } from "@/components/ui/input"
+import * as React from "react";
+import { Check, ChevronsUpDown } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { ControllerRenderProps, FieldValues } from "react-hook-form";
+import { Input } from "@/components/ui/input";
 import {
   Command,
   CommandEmpty,
   CommandGroup,
   CommandInput,
   CommandItem,
-} from "@/components/ui/command"
+} from "@/components/ui/command";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
-} from "@/components/ui/popover"
-import { CommandList } from "cmdk"
-
+} from "@/components/ui/popover";
+import { CommandList } from "cmdk";
 
 type Props = {
   form: {
@@ -30,44 +29,42 @@ type Props = {
   index: number;
   placeholder: string;
   selection: string[];
-}
+};
 
-export default function Combobox({ form, entry, index, placeholder, selection }: Props) {
-  const [open, setOpen] = React.useState(false)
-  const [value, setValue] = React.useState("")
+export default function Combobox(
+  { form, entry, index, placeholder, selection }: Props,
+) {
+  const [open, setOpen] = React.useState(false);
+  const [inputValue, setInputValue] = React.useState("");
 
   return (
     <>
-      <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild>
-          <Command>
-            <CommandInput placeholder={placeholder} />
-          </Command>
-        </PopoverTrigger>
-        <PopoverContent className="w-[200px] p-0">
-          <Command>
-            <CommandInput placeholder={placeholder} />
-            <CommandEmpty>No results...</CommandEmpty>
+      <Command>
+        <CommandInput placeholder={placeholder} onFocus={() => {setOpen(true)}}/>
+        <Popover open={open} onOpenChange={setOpen}>
+          <PopoverTrigger>
+          </PopoverTrigger>
+          <PopoverContent className="min-w-32">
             <CommandGroup>
               <CommandList>
-              {selection.map((value) => (
-                <CommandItem
-                  key={value}
-                  value={value}
-                  onSelect={(currentValue) => {
-                    setValue(currentValue === value ? "" : currentValue);
-                    setOpen(false);
-                    // form.setValue(`${entry}[${index}]`, currentValue === value ? "" : currentValue)
-                  }}
-                >
-                  {value}
-                </CommandItem>
-              ))}
+                {selection.map((value) => (
+                  <CommandItem
+                    key={value}
+                    value={value}
+                    onSelect={(currentValue) => {
+                      setInputValue(currentValue);
+                      form.setValue(`${entry}[${index}]`, currentValue);
+                      setOpen(false);
+                    }}
+                  >
+                    {value}
+                  </CommandItem>
+                ))}
               </CommandList>
             </CommandGroup>
-          </Command>
-        </PopoverContent>
-      </Popover>
+          </PopoverContent>
+        </Popover>
+      </Command>
     </>
-  )
+  );
 }
