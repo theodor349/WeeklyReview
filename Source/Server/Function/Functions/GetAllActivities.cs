daeda81720409520fd704290041e3dfdfc459a5a
Theodor Risager <theodor349@gmail.com>
2024-09-28 13:18:32 +0200

    Works

--- Source/Server/Function/Functions/GetAllActivities.cs
+++ Source/Server/Function/Functions/GetAllActivities.cs
@@ -1,3 +1,4 @@
+using Database.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
@@ -6,51 +7,35 @@ using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Shared.Services;
 using System.Net;
 
 namespace Functions.Functions;
 
-//public class GetAllActivities
-//{
-//    private readonly ILogger<GetAllActivities> _logger;
-//    private readonly IActivityService _activityService;
-
-//    public GetAllActivities(ILogger<GetAllActivities> logger, IActivityService activityService)
-//    {
-//        _logger = logger;
-//        _activityService = activityService;
-//    }
-
-//    [Function("GetAllActivitiesV1")]
-//    [OpenApiOperation(operationId: "GetAllActivitiesV1", Summary = "Retrieves all activities for a specific user", Description = "This API retrieves all activities associated with a given user GUID.")]
-//    [OpenApiParameter(name: "userGuid", In = ParameterLocation.Path, Required = true, Type = typeof(Guid), Description = "The unique identifier for the user.")]
-//    [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(List<ActivityModel>), Description = "A list of activities for the specified user.")]
-//    [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.BadRequest, Description = "Invalid GUID format or missing required parameter.")]
-//    [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.NotFound, Description = "No activities found for the specified user GUID.")]
-//    [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "x-functions-key", In = OpenApiSecurityLocationType.Header)]
-//    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = "v1/user/{userGuid}/activities")] HttpRequest req, [FromRoute] Guid userGuid)
-//    {
-//        _logger.LogInformation("C# HTTP trigger function processed a request.");
-//        var res = await _activityService.GetAll(userGuid);
-//        if (res == null || !res.Any())
-//            return new NoContentResult();
-//        return new OkObjectResult(res);
-//    }
-//}
-
-public class Function1
+public class GetAllActivities
 {
-    private readonly ILogger<Function1> _logger;
+    private readonly ILogger<GetAllActivities> _logger;
+    private readonly IActivityService _activityService;
 
-    public Function1(ILogger<Function1> logger)
+    public GetAllActivities(ILogger<GetAllActivities> logger, IActivityService activityService)
     {
         _logger = logger;
+        _activityService = activityService;
     }
 
-    [Function("Function1")]
-    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    [Function("GetAllActivitiesV1")]
+    [OpenApiOperation(operationId: "GetAllActivitiesV1", Summary = "Retrieves all activities for a specific user", Description = "This API retrieves all activities associated with a given user GUID.")]
+    [OpenApiParameter(name: "userGuid", In = ParameterLocation.Path, Required = true, Type = typeof(Guid), Description = "The unique identifier for the user.")]
+    [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(List<ActivityModel>), Description = "A list of activities for the specified user.")]
+    [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.BadRequest, Description = "Invalid GUID format or missing required parameter.")]
+    [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.NotFound, Description = "No activities found for the specified user GUID.")]
+    [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "x-functions-key", In = OpenApiSecurityLocationType.Header)]
+    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = "v1/user/{userGuid}/activities")] HttpRequest req, [FromRoute] Guid userGuid)
     {
         _logger.LogInformation("C# HTTP trigger function processed a request.");
-        return new OkObjectResult("Welcome to Azure Functions!");
+        var res = await _activityService.GetAll(userGuid);
+        if (res == null || !res.Any())
+            return new NoContentResult();
+        return new OkObjectResult(res);
     }
 }
